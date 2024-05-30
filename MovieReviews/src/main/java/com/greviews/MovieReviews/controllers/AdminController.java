d01625efd1c5b8ffd78fb90cc7663ee45a1bd415
bapanapalligopi <bapanapalligopi7@gmail.com>
2024-05-30 16:55:03 +0530

    Version-0.0.1

--- MovieReviews/src/main/java/com/greviews/MovieReviews/controllers/AdminController.java
+++ MovieReviews/src/main/java/com/greviews/MovieReviews/controllers/AdminController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://192.168.1.4:8081")
+@CrossOrigin(origins = {"http://192.168.1.4:8081", "http://localhost:5173"})
+
 public class AdminController {
     @Autowired
     AdminService adminService;
--- MovieReviews/src/main/java/com/greviews/MovieReviews/controllers/MovieController.java
+++ MovieReviews/src/main/java/com/greviews/MovieReviews/controllers/MovieController.java
@@ -9,7 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://192.168.1.4:8081")
+@CrossOrigin(origins = {"http://192.168.1.4:8081", "http://localhost:5173"})
+
 @RestController
 public class MovieController {
    @Autowired
